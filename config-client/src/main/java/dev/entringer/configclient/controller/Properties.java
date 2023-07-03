398e94f65401ef2fed85fc2c6179b2b92295935e
Fabricio Entringer Moreira <moreira@gmail.com>
2023-07-03 07:42:33 +0200

    Spring Cloud Config First version Client and Server.

--- config-client/src/main/java/dev/entringer/configclient/controller/Properties.java
+++ config-client/src/main/java/dev/entringer/configclient/controller/Properties.java
@@ -1,2 +1,20 @@
-package dev.entringer.configclient.controller;public class Properties {
+package dev.entringer.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("properties")
+public class Properties {
+
+    @Value("${app.owner}")
+    private String owner;
+
+    @GetMapping("/list")
+    public String listProperties() {
+
+        return owner;
+    }
 }
